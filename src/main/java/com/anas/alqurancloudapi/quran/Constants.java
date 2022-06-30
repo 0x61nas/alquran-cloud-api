b11b7de790655f9339a9a335df5376475f4bc7f0
Anas Elgarhy <anas.elgarhy.dev@gmail.com>
2022-06-30 16:09:35 +0200

    Create the constants class 🥰

---
+++ src/main/java/com/anas/alqurancloudapi/quran/Constants.java
@@ -0,0 +1,36 @@
+package com.anas.alqurancloudapi.quran;
+
+public class Constants {
+    /**
+     * The constant representing the naumber of all ayahs in the Quran without the bismillah.
+     */
+    public static final int AYAS_COUNT = 6236;
+    /**
+     * The constant representing the number of all ayahs in the Quran including the bismillah.
+     */
+    public static final int AYAS_COUNT_WITH_BISMILLAH = AYAS_COUNT + 112;
+    /**
+     * The constant representing the number of surahs in the Quran.
+     */
+    public static final int SURAS_COUNT = 114;
+    /**
+     * The constant representing the number of characters in the Quran.
+     */
+    public static final int CHARACTERS_COUNT = 323671;
+    /**
+     * The constant representing the number of words in the Quran.
+     */
+    public static final int WORDS_COUNT = 77439;
+    /**
+     * The constant represents the number of Meccan surahs in the Qur’an.
+     */
+    public static final int SURAS_MACCAN_COUNT = 86;
+    /**
+     * The constant represents the number of Medinan surahs in the Qur’an.
+     */
+    public static final int SURAS_MEDINAN_COUNT = 28;
+    /**
+     * The constant represents the number of sajdahs in the Qur’an.
+     */
+    public static final int SAJDAHS_COUNT = 15;
+}
