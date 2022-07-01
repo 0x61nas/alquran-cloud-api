36aff88b94199571cd2ce6a97ec5dcb28f4fc268
Anas Elgarhy <anas.elgarhy.dev@gmail.com>
2022-07-01 09:21:32 +0200

    Create the java docs for the Page class 🥰

--- src/main/java/com/anas/alqurancloudapi/quran/Page.java
+++ src/main/java/com/anas/alqurancloudapi/quran/Page.java
@@ -2,31 +2,55 @@ package com.anas.alqurancloudapi.quran;
 
 import com.anas.alqurancloudapi.quran.edition.Edition;
 
+/**
+ * It represents the page of the Quran.
+ */
 public class Page {
     private final short number;
     private final Ayah[] ayahs;
     private final Surah[] surahs;
     private final Edition edition;
 
+    // For JSON deserialization
     private Page() {
         this.number = 0;
-        this.ayahs = new Ayah[0];
-        this.surahs = new Surah[0];
+        this.ayahs = null;
+        this.surahs = null;
         this.edition = null;
     }
 
+    /**
+     * This function returns the number of the page in the Quran.
+     *
+     * @return The number of the page in the Quran.
+     */
     public short getNumber() {
         return number;
     }
 
+    /**
+     * This function returns an array of Ayahs in the page.
+     *
+     * @return An array of Ayah objects.
+     */
     public Ayah[] getAyahs() {
         return ayahs;
     }
 
+    /**
+     * This function returns an array of Surahs in the page.
+     *
+     * @return An array of Surah objects.
+     */
     public Surah[] getSurahs() {
         return surahs;
     }
 
+    /**
+     * This function returns the edition of the page.
+     *
+     * @return The edition of the page.
+     */
     public Edition getEdition() {
         return edition;
     }
