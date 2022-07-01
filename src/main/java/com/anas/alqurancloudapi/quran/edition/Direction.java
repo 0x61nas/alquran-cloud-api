15f9fcdb0171d46fc93ddbdb444f68a07ff1e60b
Anas Elgarhy <anas.elgarhy.dev@gmail.com>
2022-07-01 07:17:19 +0200

    Create the java docs for the Direction enum 😃

--- src/main/java/com/anas/alqurancloudapi/quran/edition/Direction.java
+++ src/main/java/com/anas/alqurancloudapi/quran/edition/Direction.java
@@ -2,6 +2,10 @@ package com.anas.alqurancloudapi.quran.edition;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+/**
+ * The direction of the text in the edition.
+ * Right-to-left or left-to-right.
+ */
 public enum Direction {
     @JsonProperty("rtl")
     RTL,
