5a7ddca9ae67542bdfb59dd820f1b4e74e42f3ad
Anas Elgarhy <anas.elgarhy.dev@gmail.com>
2022-07-01 07:40:27 +0200

    Create the java docs for the EditionFormat enum 🥰

--- src/main/java/com/anas/alqurancloudapi/quran/edition/EditionFormat.java
+++ src/main/java/com/anas/alqurancloudapi/quran/edition/EditionFormat.java
@@ -2,6 +2,10 @@ package com.anas.alqurancloudapi.quran.edition;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+/**
+ * The format of the edition.
+ * text or audio (maybe including text also).
+ */
 public enum EditionFormat {
     @JsonProperty("text")
     TEXT,
